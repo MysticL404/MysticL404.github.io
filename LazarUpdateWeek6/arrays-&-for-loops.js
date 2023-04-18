888e7c939975102836f3462c773b4fa778960bdf
MysticL404 <127101660+MysticL404@users.noreply.github.com>
2023-04-18 16:24:59 +1000

    Add files via upload

---
+++ LazarUpdateWeek6/arrays-&-for-loops.js
@@ -0,0 +1,12 @@
+var fruit1 = "banana";
+var fruit2 = "pineapple";
+var fruit3 = "apple";
+var fruit4 = "peach";
+var fruit5 = "orange";
+
+var fruitArray = ["banana", "pineapple", "apple", "peach", "orange"];
+
+var mixedArray = [42, "pineapple", true, [3, 6, "nine"], "orange"];
+
+console.log("I'm going to help test the array");
+
